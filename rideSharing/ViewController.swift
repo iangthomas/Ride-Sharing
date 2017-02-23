5be578e266606829f63914fcac7548170bfd6927
iangthomas <iangthomas@mac.com>
2017-02-23 14:28:59 -0800

    Initial Commit

---
+++ rideSharing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rideSharing
+//
+//  Created by Ian Thomas on 2/23/17.
+//  Copyright © 2017 Geodex Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
